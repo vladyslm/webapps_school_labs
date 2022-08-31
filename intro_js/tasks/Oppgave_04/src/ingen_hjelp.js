575fe590173852736931afae7a9ab0a588c58eb6
Vladyslav <vladyslav@pop-os.localdomain>
2022-08-31 23:26:39 +0200

    done: task 4

--- intro_js/tasks/Oppgave_04/src/ingen_hjelp.js
+++ intro_js/tasks/Oppgave_04/src/ingen_hjelp.js
@@ -1,11 +1,80 @@
-import './styles.css';
+// import './styles.css';
 
-// TODO: Bruk getElementById til å hente HTML-elementene med #id
-// TODO: Bruk querySelector til å hente knappen
-// TODO: Lag en liste med tallene som skal sorteres
-// TODO: Lag en funksjon for å skrive ut tallene som skal sorteres
-// TODO: Lag en funksjon for å skrive ut input felter bruker kan skrive tallene i
-// TODO: Lag en funksjon for å hente ut det brukeren har skrevet inn
-// TODO: Lag en funksjon for å sjekke om tallene er sortert riktig av bruker
-// TODO: Lag en funksjon som "lager UI" --Nødvendig?
-// TODO: Lytt til 'click'-event og trigg checkNumberSeq ved klikk
+const NUMBER_OF_EL = 5;
+const MIN = 0;
+const MAX = 10;
+
+const CORRECT_MSG = "Correct!";
+const FAILED_MSG = "Wrong!";
+
+function generateRandomNumbersInRange(num, from, to) {
+    const arr = [];
+    for (let i = 0; i < num; i++) {
+        arr.push(Math.floor(Math.random() * (to - from)) + from);
+    }
+    return arr;
+}
+
+const generatedNumbers = generateRandomNumbersInRange(NUMBER_OF_EL, MIN, MAX);
+
+const mainHtml = document.getElementById("main");
+const submitBtn = document.querySelector("button");
+const guessUl = document.getElementById("guess");
+const numbers = document.getElementById("numbers");
+
+numbers.style.display = "flex";
+numbers.style.flexDirection = "column"
+numbers.style.width = "50px"
+
+
+function displayNumbers() {
+    generatedNumbers.forEach((el) => {
+        const li = document.createElement("li");
+        li.textContent = el;
+        guessUl.appendChild(li);
+
+        const fragment = document.createDocumentFragment();
+        const inputLi = fragment.appendChild(
+            document.createElement("input")
+        )
+        numbers.appendChild(inputLi);
+    })
+
+}
+
+
+function renderApp() {
+    displayNumbers(generatedNumbers);
+}
+
+
+function compareAnswers(users, correct) {
+    let isCorrect = false;
+    for (let i = 0; i < correct.length; i++) {
+        if (correct[i] !== users[i]) {
+            return isCorrect;
+        }
+    }
+    return !isCorrect;
+}
+
+function handleAnswers() {
+    const correctAnswers = generatedNumbers.sort().map((el) => `${el}`);
+    const numberEl = Array.from(numbers.children);
+    const userAnswers = [];
+    numberEl.forEach((el) => {
+        userAnswers.push(el.value);
+    })
+
+
+    const isCorrect = compareAnswers(userAnswers, correctAnswers);
+    const msg = isCorrect ? CORRECT_MSG : FAILED_MSG;
+
+    const msgEl = document.createElement("p");
+    msgEl.textContent = msg;
+    mainHtml.appendChild(msgEl);
+}
+
+submitBtn.addEventListener("click", handleAnswers);
+
+renderApp();
