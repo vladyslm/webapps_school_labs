97f3a070e28b5e66aff9da11226d4ad5b9a77517
Vladyslav <vladyslav@pop-os.localdomain>
2022-08-31 21:00:24 +0200

    done: task 1

--- intro_js/tasks/Oppgave_01/src/start.js
+++ intro_js/tasks/Oppgave_01/src/start.js
@@ -1,13 +1,17 @@
-import './styles.css';
+// import './styles.css';
 
 // Statisk tekst som brukes til å søke i
 const text =
   'Baby cliche unicorn brooklyn farm-to-table. Salvia semiotics hella literally paleo humblebrag bushwick letterpress messenger bag pork belly brooklyn authentic vexillologist. Gastropub sustainable banjo, shaman snackwave viral air plant ramps health goth. Edison bulb vegan microdosing, tote bag unicorn skateboard disrupt copper mug four loko sustainable whatever cloud bread slow-carb lumbersexual four dollar toast. Waistcoat lomo hammock vape shabby chic sartorial yr godard pok pok fashion axe organic migas. Quinoa yr vexillologist intelligentsia verylongwordthatislong neutra mixtape YOLO XOXO listicle letterpress farm-to-table beard.';
 
 const longestWord = () => {
-  // TODO: Gjør om text til array eks. ['Baby', 'cliche']
-  // TODO: Velg første ord så du har noe å sammenlikne med
-  // TODO: Gå igjennom alle ordene og oppdater hvis nytt ord er lengre
+    const words = text.split(" ");
+    let word = "";
+    words.forEach((el) => {
+        if (el.length > word.length) word = el;
+    })
+
+    return word
 };
 
 console.log(longestWord());
